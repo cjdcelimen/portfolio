86d3403e56d8349b042b7beb9e631562b513f5ff
CJ Celimen <cjdcelimen@gmail.com>
2020-10-10 14:34:51 +0800

    Updates

---
+++ precache-manifest.1d5b5530e991671c3359c267af72cf0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b009778503c4f46a6e4329cd5abb7ff5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "406d645e4b0bfef61265",
+    "url": "/portfolio/static/css/main.0007f71f.chunk.css"
+  },
+  {
+    "revision": "a6cc19068174ca88ce14",
+    "url": "/portfolio/static/js/2.9c3b93da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.9c3b93da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "406d645e4b0bfef61265",
+    "url": "/portfolio/static/js/main.2dbb5d02.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
