5052dfbcb1a61ee2240339d0559916620b53a452
CJ Celimen <cjdcelimen@gmail.com>
2020-10-11 22:04:33 +0800

    Updates

--- precache-manifest.82c86bee151a00b1b7f10e48b291a1aa.js
+++ precache-manifest.82c86bee151a00b1b7f10e48b291a1aa.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "be3c9daa0d253adba204df2c8d3a21ef",
+    "revision": "93817dbf25d3de121a56a9ce351bc7e5",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "59c8f12ed63eb1b5ffed",
-    "url": "/portfolio/static/css/main.5ecfd988.chunk.css"
+    "revision": "b24f1a8c6b78d6e618e3",
+    "url": "/portfolio/static/css/main.ad77df4a.chunk.css"
   },
   {
     "revision": "a8d91a47f67379c3c30f",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.26538d3b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "59c8f12ed63eb1b5ffed",
-    "url": "/portfolio/static/js/main.b7207b6d.chunk.js"
+    "revision": "b24f1a8c6b78d6e618e3",
+    "url": "/portfolio/static/js/main.2413868d.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.8c87c57d169a373b530129cdd5a42997.js"
+  "/portfolio/precache-manifest.82c86bee151a00b1b7f10e48b291a1aa.js"
 );
 
 self.addEventListener('message', (event) => {
