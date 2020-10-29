b48c1041b6855cd5f5eb4304ef112566418929f1
CJ Celimen <cjdcelimen@gmail.com>
2020-10-29 21:25:13 +0800

    Initial basic contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,10 +2,45 @@ import React from 'react';
 
 export const Contact = () => {
   return (
-    <div className='text-wrapper'>
-      <div className='swipe-title'>
-        <h1>Contact</h1>
+    <>
+      <div className='contact-wrapper'>
+        <h1 className='contact-message'>For the appreciation of design and continuous innovation, let's collaborate.</h1>
       </div>
-    </div>
+      <div className='contact-wrapper-container'>
+        <div className='contact-wrapper-form'>
+          <h2>Contact Me</h2>
+          <div className='row100'>
+            <div className='col'>
+              <div className='inputBox'>
+                <input type='text' name=''></input>
+                <span className='text'>Name</span>
+                <span className='line'></span>
+              </div>
+            </div>
+            <div className='col'>
+              <div className='inputBox'>
+                <input type='text' name=''></input>
+                <span className='text'>Email</span>
+                <span className='line'></span>
+              </div>
+            </div>
+          </div>
+          <div className='row100'>
+            <div className='col'>
+              <div className='inputBox textarea'>
+                <textarea></textarea>
+                <span className='text'>Message</span>
+                <span className='line'></span>
+              </div>
+            </div>
+          </div>
+          <div className='row100'>
+            <div className='col'>
+              <input className='submit' type='submit' value='Send'></input>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
